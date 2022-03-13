3ac735388843cb6b92b1e8121c2fad95c831905e
Amallia01 <200411100025@student.trunojoyo.ac.id>
2022-03-13 20:38:23 +0700

    membuat file variabel.php

---
+++ variabel.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Tugas PAW nomor 2</title>
+    </head>    
+    <body>
+	<h2 align="left">Variabel</h2> 
+        <?php
+		//data variabel
+        $a = 4 ;
+        $b = 5 ;
+        $c = 8 ;
+        $d = $a * $c ;
+        $e = ($a * 5)/4 ;
+        $f = ($b+$c-$a)/3 ;
+
+        //menampilkan data
+        echo "Variabel a : {$a} <br>";
+        echo "Variabel b : {$b} <br>";
+        echo "Variabel c : {$c} <br>";
+        echo "Variabel d : {$d} <br>";
+        echo "Variabel e : {$e} <br>";
+        echo "Variabel f : {$f} <br>";
+        ?> 
+    </body>
+</html>
