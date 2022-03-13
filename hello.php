8d617b3d879a38018fae74d207c3eacb97f50db2
Amallia01 <200411100025@student.trunojoyo.ac.id>
2022-03-13 20:37:53 +0700

    membuat file hello.php

---
+++ hello.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Tugas PAW nomor 1</title>
+    </head>    
+    <body>
+        <?php
+        $nama = "Amallia Tiara Putri";//variabel untuk menampilkan nama
+        $nim = "200411100025";//variabel untuk menampilkan nim
+
+        //menampilkan data
+        echo "Nama : {$nama} <br>";
+        echo "NIM : {$nim} <br>";
+        ?>
+    </body>
+</html>
